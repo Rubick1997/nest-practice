a10815ecc7b06900313f7e27e28a48c89ff35edd
Rustam Kolumbayev <rustamkolumbayev@Rustams-MacBook-Pro-2.local>
2022-03-06 13:26:14 -0800

    providing with role+banning users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
 import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { Roles } from "src/auth/roles-auth.decorator";
 import { RolesGuard } from "src/auth/roles.guard";
+import { AddRoleDto } from "./dto/add-role.dto";
+import { BanUserDto } from "./dto/ban-user.dto";
 
 import { CreateUserDto } from "./dto/create-user.dto";
 import { User } from "./users.model";
@@ -26,5 +28,23 @@ export class UsersController {
   @Get()
   getAll() {
     return this.usersService.getAllUsers();
+  }
+
+  @ApiOperation({ summary: "Provide with role" })
+  @ApiResponse({ status: 200 })
+  @Roles("ADMIN")
+  @UseGuards(RolesGuard)
+  @Post("/role")
+  addRole(@Body() dto: AddRoleDto) {
+    return this.usersService.addRole(dto);
+  }
+
+  @ApiOperation({ summary: "Ban Role" })
+  @ApiResponse({ status: 200 })
+  @Roles("ADMIN")
+  @UseGuards(RolesGuard)
+  @Post("/ban")
+  ban(@Body() dto: BanUserDto) {
+    return this.usersService.ban(dto);
   }
 }
